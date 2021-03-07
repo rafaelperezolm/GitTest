a0b682df5c0b4241d1e69ab0bb4f469534c2f404
Frick <rafaelperezolm@icloud.com>
2021-03-07 14:18:30 -0600

    Initial GitTest project

--- app/src/main/java/com/rafaelperezolm/gittest/MainActivity.kt
+++ app/src/main/java/com/rafaelperezolm/gittest/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        //Comment
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
